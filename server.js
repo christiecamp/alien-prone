90f15a7d6b8281aecf5667916586468557b43fce
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-25 18:40:04 -0500

    working in server

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const path = require('path')
 const express = require('express');
+//middleware module in Express that allows you to create sessions in your web application & stores session data on the server side, using a variety of different storage options, and allows you to track the activity of a user across requests.
 const session = require('express-session');
+//a Handlebars view engine for Express which doesn't suck
 const exphbs = require('express-handlebars');
 const routes = require('./lib/controllers');
+//custom helpers
 const helpers = require('./lib/utils/helpers');
 //sequelize connection
 const sequelize = require('./lib/config/connection');
+//SQL session store
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const probe = express();
 const PORT = process.env.PORT || 3013;
 
-
-/////// add detailed comments for all of the below ////////////
-
 //handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 //session object
+//session - a mechanism for maintaining state between requests in a stateless web environment; stores data about a user’s interaction with the application, such as their preferences, shopping cart items, or authenticated status; this information is stored on the server, and a unique session identifier is used to associate the user with their data.
 const sess = {
     secret: 'Alien abduction secret',
     //HTTP cookie (web cookie, browser cookie) is a small piece of data that a server sends to a user's web browser. 
@@ -39,8 +41,6 @@ probe.use(session(sess)); //pass session
 //inform Express.js on which template engine to use
 probe.engine('handlebars', hbs.engine);
 probe.set('view engine', 'handlebars');
-
-//////////////////////////////////////////////////////////////////
 
 probe.use(express.json());
 probe.use(express.urlencoded({ extended: true }));
