e58db9634cdc472a009eb0f9ed0cec6207102af3
christiecamp <christiecamphoto@gmail.com>
2023-12-01 04:57:17 -0500

    fixed bug - fixed format time in helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,6 @@ module.exports = {
         // return date.toLocalTimeString();
     },
     format_date: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} + ${new Date(date).getHours()} `;
+        return `${new Date(date).getMonth() + 1}.${new Date(date).getDate()}.${new Date(date).getFullYear()}  ${new Date(date).getHours().toString().padStart(2, '0')}.${new Date(date).getMinutes().toString().padStart(2, '0')}.${new Date(date).getSeconds().toString().padStart(2, '0')}`;
     },
 };
